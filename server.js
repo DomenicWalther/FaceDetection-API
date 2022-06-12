5644950cdc914f52b043f3192c7a296724812438
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2022-06-12 02:08:42 +0200

    Fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const db = require("knex")({
   client: "pg",
   connection: {
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.DATABASE_URL,
     ssl: true,
   },
 });
